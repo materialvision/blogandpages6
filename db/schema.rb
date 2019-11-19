1db56f279472ae268fb9144654eeaa0fb953c24b
Espen Sommer Eide <espensommer@gmail.com>
2019-11-19 22:23:12 +0100

    etter 601

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_19_082214) do
+ActiveRecord::Schema.define(version: 2019_11_19_211925) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
