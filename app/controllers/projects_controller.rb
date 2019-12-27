85ba0185ba0462300b93af7b28e8febda219054d
Espen Sommer Eide <espensommereide@EspensMBP.local>
2019-12-27 20:03:04 +0100

    Final before deploy

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,7 +4,7 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.json
   def index
-    @projects = Project.all
+    @projects = Project.all.sort_by { |item| item.importance }
   end
 
   # GET /projects/1
