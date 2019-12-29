dcadca578a06c437427c54f119ff80e0b0efd717
Espen Sommer Eide <espensommereide@EspensMBP.local>
2019-12-29 19:31:09 +0100

    authent

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticity_token
+  protect_from_forgery prepend: true, with: :exception
   helper_method :current_user
   def current_user
     if session[:user_id]
